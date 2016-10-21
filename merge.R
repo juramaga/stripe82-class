2d6a060d13ff5d68fa226b90f9c6190601c27f46
James Long <jlong@stat.tamu.edu>
2016-10-21 12:36:53 -0400

    version controlling todo

--- merge.R
+++ merge.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
 ## read in source catalog
-dat <- read.table("stripe82candidateVar_v1.1.dat")
+dat <- read.table("data/stripe82candidateVar_v1.1.dat")
 col.names <- c("ID","ra","dec","P","r","ug","gr","ri","iz","gN","gAmpl","rN","rAmpl","iN","iAmpl","zQSO","MiQSO")
 colnames(dat) <- col.names
 dat$class <- "unknown" ## initial classification
@@ -9,8 +9,8 @@ dat$class <- "unknown" ## initial classification
 ## qsos have zQSO!=-9.9
 dat$class[dat$zQSO!=-9.9] <- "QSO"
 
-## RR Lyrae were identified by sesar
-rrlyrae <- read.table("apj326724t2_mrt.txt",skip=42)
+## RR Lyrae (from sesar 2010)
+rrlyrae <- read.table("data/apj326724t2_mrt.txt",skip=42)
 rrlyrae <- rrlyrae[,1:2]
 rrlyrae[,2] <- paste0("rr_",rrlyrae[,2])
 names(rrlyrae) <- c("ID","classrr")
@@ -20,4 +20,8 @@ dat <- merge(dat,rrlyrae,all=TRUE)
 dat$class[!is.na(dat$classrr)] <- dat$classrr[!is.na(dat$classrr)]
 dat$classrr <- NULL
 
-## eclipsing binary sources
+## TODO: merge eclipsing binary sources
+
+
+## output catalog
+write.table(dat,"catalog.txt",row.names=FALSE,quote=FALSE)
