bc7d159a50aed73b5ad4641d5da201b46f2532f8
James Long <jlong@stat.tamu.edu>
2016-10-31 18:11:33 -0400

    automating downloading of all data

---
+++ download.py
@@ -0,0 +1,41 @@
+## downloads all data necessary for creating catalog
+## by James Long
+
+import urllib2
+import os
+import shutil
+
+d = "data" ## name of folder to store data files
+
+
+#### STEP 1: REMOVE AND CREATE DATA FOLDER
+
+if os.path.exists(d):
+    shutil.rmtree(d)
+
+os.makedirs(d)
+
+
+#### STEP 2: DOWNLOAD SDSS STRIPE 82 CATALOG AND LIGHTCURVES
+## only has quasar classifications
+## lightcurves only necessary for visualizing data / checking classifications
+
+## http://www.astro.washington.edu/users/ivezic/sdss/catalogs/S82variables.html
+
+
+
+#### STEP 3: DOWNLOAD CLASSIFICATIONS ASSIGNED IN VARIOUS WORKS
+## urls    : location of class files
+## fnames  : list of files names to store different classifications
+fnames = ["rrlyrae.txt",
+          "eclipsing_binary.txt"]
+urls = ["http://iopscience.iop.org/0004-637X/708/1/717/suppdata/apj326724t2_mrt.txt",
+        "http://iopscience.iop.org/0004-637X/731/1/17/suppdata/apj383813t2_ascii.txt"]
+for ii in range(len(fnames)):
+    response = urllib2.urlopen(urls[ii])
+    html = response.read()
+    f = open(d + "/" + fnames[ii], 'w')
+    f.write(html)
+    f.close()
+
+
