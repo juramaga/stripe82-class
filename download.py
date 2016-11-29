95920130d0e38b29c7d12746f7b9470db223b5ed
James Long <jlong@stat.tamu.edu>
2016-11-29 00:39:36 -0500

    corrected download.py to work with washington data

--- download.py
+++ download.py
@@ -1,11 +1,15 @@
 ## downloads all data necessary for creating catalog
 ## by James Long
+## tested on python 2.7
+
 import urllib
 import shutil
 import os
-import zipfile
+import gzip
+import tarfile
 
 d = "data" ## name of folder to store data files
+LCfolder = "AllLCs" ## subfolder within d where lightcurves files stored
 
 #### STEP 1: REMOVE AND CREATE DATA FOLDER
 if os.path.exists(d):
@@ -13,28 +17,38 @@ if os.path.exists(d):
 
 os.makedirs(d)
 
+
+
 #### STEP 2: DOWNLOAD SDSS STRIPE 82 CATALOG AND LIGHTCURVES
 ## only has quasar classifications
 ## lightcurves only necessary for visualizing data / checking classifications
-## ::: usually hosted at :::
-## http://www.astro.washington.edu/users/ivezic/sdss/catalogs/S82variables.html
-## http://www.astro.washington.edu/users/ivezic/sdss/catalogs/stripe82candidateVar_v1.1.dat.gz
-## ::: but currently these servers are down so downloading from james long's website :::
 url = "http://www.astro.washington.edu/users/ivezic/sdss/catalogs/"
 ## url = "http://www.stat.tamu.edu/~jlong/"   ## mirrored here, can try if above link is not working
-fnames = ["stripe82candidateVar_v1.1.zip","AllLCs.zip"]
-for fname in fnames:
-    print "downloading " + fname + " . . ."
-    urllib.urlretrieve(url + fname, d + '/' + fname)
-    zip_ref = zipfile.ZipFile(d + '/' + fname, 'r')
-    zip_ref.extractall(d)
-    zip_ref.close()
+## download and extract catalog
+fname = "stripe82candidateVar_v1.1.dat.gz"
+print "downloading " + fname + " . . ."
+urllib.urlretrieve(url + fname, d + '/' + fname)
+inF = gzip.open(d + '/' + fname, 'rb')
+outF = open(d + '/stripe82candidateVar_v1.1.dat','wb')
+outF.write(inF.read())
+inF.close()
+outF.close()
+## download and extract lightcurves
+fname = "AllLCs.tar.gz"
+print "downloading " + fname + " . . ."
+urllib.urlretrieve(url + fname, d + '/' + fname)
+tar = tarfile.open(d + '/' + fname)
+os.makedirs(d + '/' + LCfolder)
+tar.extractall(d + '/' + LCfolder)
+tar.close()
 
+
+    
 #### STEP 3: DOWNLOAD CLASSIFICATIONS ASSIGNED IN VARIOUS WORKS
 ## urls    : location of class files
 ## fnames  : list of files names to store different classifications
 ##
-## we will extend these list as we collect classifications from more sources
+## extend this list as gather more sources with classifications
 fnames = ["rrlyrae.txt",
           "eclipsing_binary.txt"]
 urls = ["http://iopscience.iop.org/0004-637X/708/1/717/suppdata/apj326724t2_mrt.txt",
