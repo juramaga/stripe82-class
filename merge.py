bdace3a24e80b1388a35371ac9125a437b01b8e2
christinawlindberg <christina.lindberg@live.com>
2017-07-17 16:12:36 -0500

    added catalina merger and standardized class names

--- merge.py
+++ merge.py
@@ -22,6 +22,74 @@ data = pd.read_table("data/stripe82candidateVar_v1.1.dat",header=None,sep=" ",
 del data[17]
 data.columns = ["ID","ra","dec","P","r","ug","gr","ri","iz","gN","gAmpl","rN","rAmpl","iN","iAmpl","zQSO","MiQSO"]
 data['cl'] = "unknown"
+data['Cat_P'] = 'n/a' #Catalina Period
+data['Cat_mag'] = 'n/a' #Catalina V magnitude
+
+############################BEGINNING OF CATALINA MERGER##########################################
+# merge catalina catalog 
+print "merging catalina catalog . . . "
+#creates a nice data array
+catalina = pd.read_table("data-controlled/catalina-catalog-converted.txt",
+                       header=1,delim_whitespace=True,
+                       skipinitialspace=True,comment="#",names=['Catalina_Surveys_ID','Numerical_ID','ra',
+                             'dec','V_(mag)','Period_(days)','Amplitude','Number_Obs','Var_Type','ra_deg','dec_deg'])
+
+#create the catalina coordinate list
+c1 = SkyCoord(ra = catalina['ra_deg'], dec=catalina['dec_deg'], unit=(u.degree,u.degree))
+
+#create the stripe-82 coordinate list
+catalog = SkyCoord(ra=np.array(data['ra'])*u.degree, dec=np.array(data['dec'])*u.degree)
+
+data['g'] = data['gr'] + data['r'] 
+data['v'] = data['g'] - 0.5784*data['gr'] - 0.0038
+
+#compare the two
+idx, d2d, d3d = c1.match_to_catalog_sky(catalog)
+#eliminates all matches that have too big of a separation
+idy =  np.where((d2d.arcsec) < 1.5) ####make error check####
+
+#this was included in the code
+#I merely modified it for the catalina catalog
+catalina_new = {'ra': np.array(data['ra'])[idx[idy]], 'dec': np.array(data['dec'])[idx[idy]], 
+                'classecl':np.array(catalina['Var_Type'])[idy], 'cat_p':np.array(catalina['Period_(days)'])[idy],
+                'v_mag':np.array(catalina['V_(mag)'])[idy]}
+catalina_new = pd.DataFrame(data=catalina_new)
+
+#add the new data to the old catalog
+data = pd.merge(data,catalina_new,on=['ra','dec'],how='left')
+ix = np.invert(pd.isnull(data['classecl']))
+data['cl'][ix] = data['classecl'][ix]
+data['Cat_P'][ix] = data['cat_p'][ix]
+data['Cat_mag'][ix] = data['v_mag'][ix]
+del data['classecl']
+del data['cat_p']
+del data['v_mag']
+
+###Most types found in https://www.aavso.org/vsx/index.php?view=about.vartypes
+data['temp_cl'] = ''
+data['temp_cl'].loc[data['cl']=='unknown']='unknown'
+data['temp_cl'].loc[data['cl']==1]='ew' #W Ursae Majoris-type eclipsing variables
+data['temp_cl'].loc[data['cl']==2]='ea' #β Persei-type (Algol) eclipsing systems
+data['temp_cl'].loc[data['cl']==3]='eb' #β Lyrae-type eclipsing systems
+data['temp_cl'].loc[data['cl']==4]='rr_ab' #RR Lyrae variables with asymmetric light curves
+data['temp_cl'].loc[data['cl']==5]='rr_c' #RR Lyrae variables with nearly symmetric
+data['temp_cl'].loc[data['cl']==6]='rr_d' #Double-mode RR Lyrae stars which pulsate...
+data['temp_cl'].loc[data['cl']==7]='bl' #RR Lyrae stars showing the Blazhko effect.
+data['temp_cl'].loc[data['cl']==8]='rs' #non-eclipsing RS CVn stars
+data['temp_cl'].loc[data['cl']==9]='acep' #Anomalous Cepheids
+data['temp_cl'].loc[data['cl']==10]='cep-ii' ####need to confirm which star this is...
+data['temp_cl'].loc[data['cl']==11]='hads' #High Amplitude δ Scuti stars
+data['temp_cl'].loc[data['cl']==12]='lads'
+data['temp_cl'].loc[data['cl']==13]='lpv' #Long Period Variables 
+data['temp_cl'].loc[data['cl']==14]='ell' #ellipsoidal binary system
+data['temp_cl'].loc[data['cl']==15]='hump' ####????
+data['temp_cl'].loc[data['cl']==16]='pceb' #post common envelope binary planetary system
+data['temp_cl'].loc[data['cl']==17]='ea_up' ####????
+
+data['cl'] = data['temp_cl']
+del data['temp_cl']
+
+#######################################END OF CATALINA MERGER#####################################
 
 ## QSOs have redshift estimates > 0
 print "QSO . . . "
@@ -114,6 +182,41 @@ data = pd.merge(data,eclipsing_new,on=['ra','dec'],how='left')
 ix = np.invert(pd.isnull(data['classecl']))
 data['cl'][ix] = data['classecl'][ix]
 del data['classecl']
+
+############Standardizing naming###############
+print "standardizing classification names . . ."
+#if you want to rename anything, just write what it's called and what you want to replace it with
+
+#eg.
+#data['temp_cl'].loc[data['cl']=='what it is called']='what you want to call it'
+
+data['temp_cl'] = ''
+data['temp_cl'].loc[data['cl']=='unknown']='unknown'
+data['temp_cl'].loc[data['cl']=='ew']='ew' #W Ursae Majoris-type eclipsing variables
+data['temp_cl'].loc[data['cl']=='ea']='ea' #β Persei-type (Algol) eclipsing systems
+data['temp_cl'].loc[data['cl']=='eb']='eb' #β Lyrae-type eclipsing systems
+data['temp_cl'].loc[data['cl']=='rr_ab']='rr_ab' #RR Lyrae variables with asymmetric light curves
+data['temp_cl'].loc[data['cl']=='rr_c']='rr_c' #RR Lyrae variables with nearly symmetric
+data['temp_cl'].loc[data['cl']=='rr_d']='rr_d' #Double-mode RR Lyrae stars which pulsate...
+data['temp_cl'].loc[data['cl']=='bl']='bl' #RR Lyrae stars showing the Blazhko effect.
+data['temp_cl'].loc[data['cl']=='rs']='rs' #non-eclipsing RS CVn stars
+data['temp_cl'].loc[data['cl']=='acep']='acep' #Anomalous Cepheids
+data['temp_cl'].loc[data['cl']=='cep-ii']='cep-ii' ####need to confirm which star this is...
+data['temp_cl'].loc[data['cl']=='hads']='hads' #High Amplitude δ Scuti stars
+data['temp_cl'].loc[data['cl']=='lads']='lads'
+data['temp_cl'].loc[data['cl']=='lpv']='lpv' #Long Period Variables 
+data['temp_cl'].loc[data['cl']=='ell']='ell' #ellipsoidal binary system
+data['temp_cl'].loc[data['cl']=='hump']='hump' ####????
+data['temp_cl'].loc[data['cl']=='pceb']='pceb' #post common envelope binary planetary system
+data['temp_cl'].loc[data['cl']=='ea_up']='ea_up' ####????
+data['temp_cl'].loc[data['cl']=='RRAB']='rr_ab'
+data['temp_cl'].loc[data['cl']=='RRmf']='rr_mf'
+data['temp_cl'].loc[data['cl']=='RRC']='rr_c'
+data['temp_cl'].loc[data['cl']=='RRdm']='rr_dm'
+data['temp_cl'].loc[data['cl']=='RRu']='rr_u'
+
+data['cl'] = data['temp_cl']
+del data['temp_cl']
 
 ## output catalog
 print "writing catalog . . ."
