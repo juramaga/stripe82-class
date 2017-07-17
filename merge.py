89e632b1096f6d93131c000f280e9cc0f3171fc4
christinawlindberg <christina.lindberg@live.com>
2017-07-17 16:40:26 -0500

    Update merge.py

--- merge.py
+++ merge.py
@@ -192,6 +192,7 @@ print "standardizing classification names . . ."
 
 data['temp_cl'] = ''
 data['temp_cl'].loc[data['cl']=='unknown']='unknown'
+data['temp_cl'].loc[data['cl']=='QSO']='QSO'
 data['temp_cl'].loc[data['cl']=='ew']='ew' #W Ursae Majoris-type eclipsing variables
 data['temp_cl'].loc[data['cl']=='ea']='ea' #β Persei-type (Algol) eclipsing systems
 data['temp_cl'].loc[data['cl']=='eb']='eb' #β Lyrae-type eclipsing systems
